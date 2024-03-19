3e31f5bbac3cdd647e49c211d7cdb1c89e1d74ff
baram55 <asd7973@gmail.com>
2024-03-19 23:07:37 +0900

    Fix : package.lock 충돌 수정

--- src/app/(test)/notes/page.tsx
+++ src/app/(test)/notes/page.tsx
@@ -1,15 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
+// import { Database } from "./database.types";
 
 export default async function Notes() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  const supabase = createClient(supabaseUrl!, supabaseKey!);
-
-  const { data, error } = await supabase.from("todos").select();
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+  const { data: todos, error } = await supabase.from("todos").select("*");
 
   if (error) {
-    return <p>error</p>;
+    <p>error</p>;
   }
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <p>{JSON.stringify(todos, null, 2)}</p>;
 }
