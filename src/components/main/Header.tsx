f80c8d9a180368aaffe810378665c30d572b1679
gkstnadl <gkstnadl12@naver.com>
2024-03-20 16:00:00 +0900

    Design: Header 기능 및 디자인 1차구현

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -4,17 +4,31 @@ import SignInOutButton from "@/components/main/SignInOutButton";
 
 const Header = () => {
   return (
-    <header>
-      <section>
-        <Link href="/">Home</Link>
-      </section>
-      <nav>
-        <Link href="/board">Board</Link>
-        <Link href="/fortune">Fortune</Link>
-        <Link href="/starsign">Starsign</Link>
-        <Link href="/company">Company</Link>
-      </nav>
-      <SignInOutButton />
+    <header className="fixed z-50 w-full top-0 bg-zinc-950">
+      <div className="navbar">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl">Starry-night</a>
+        </div>
+        <div className="navbar-center lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <Link href="/board">Board</Link>
+            </li>
+            <li>
+              <Link href="/fortune">Fortune</Link>
+            </li>
+            <li>
+              <Link href="/starsign">Starsign</Link>
+            </li>
+            <li>
+              <Link href="/company">Company</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <SignInOutButton />
+        </div>
+      </div>
     </header>
   );
 };
