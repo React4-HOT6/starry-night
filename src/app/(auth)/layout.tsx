6d6237df347d960e4fdc696dcb74187a40382b2a
heechul94 <yhcu3517@gmail.com>
2024-03-22 23:37:59 +0900

    Design: 로그인 회원가입 페이지 반응형 디자인

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,8 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex h-screen flex-col justify-center items-center">
-      {children}
-      <p className="fixed bottom-12">by Team HOT6</p>
+    <main className="flex flex-col justify-center items-center min-h-screen">
+      <div>{children}</div>
+      <div className="fixed bottom-5">by Team HOT6</div>
     </main>
   );
 };
