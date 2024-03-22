cfc63360f117dfe75392372169bb9391c20a632e
yeriniii <yerini000303@naver.com>
2024-03-22 17:25:51 +0900

    Refactor:프로필과 포스트 component 분리

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,14 +2,14 @@
 
 import { useState } from "react";
 import { useEffect } from "react";
-import { HoverEffect } from "@/components/mypage/CardHoverUI";
 import { supabase } from "@/libs/supabase/client";
 import { Tables } from "@/types/database.types";
 import { calculateBirthZodiac } from "@/components/fortune/BirthZodiac";
-import { Aries } from "@/components/starsign/StarSigns";
+import Profile from "@/components/mypage/Profile";
+import PostSection from "@/components/mypage/PostSection";
 type Board = Tables<"board">;
 const MyPage = () => {
-  // const [isAvatarUploaded, setIsAvatarUploaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isEdited, setIsEdited] = useState(false);
   const [birth, setBirth] = useState("");
   const [nickname, setNickname] = useState("");
@@ -22,6 +22,7 @@ const MyPage = () => {
   }, []);
   //프로필정보랑 게시글 불러오기
   const fetchPostsAndProfile = async () => {
+    setIsLoading(true);
     try {
       const {
         data: { user },
@@ -43,7 +44,13 @@ const MyPage = () => {
         // 프로필 이미지가 없을 경우 기본 이미지 URL 설정
         avatarUrl = URL.createObjectURL(avatarResponse.data);
       }
-      setAvatarUrl(avatarUrl);
+      const img = new Image();
+      img.src = avatarUrl;
+
+      img.onload = () => {
+        setAvatarUrl(avatarUrl);
+      };
+      // setAvatarUrl(avatarUrl);
 
       if (user && user.id) {
         const { data: posts } = await supabase
@@ -54,9 +61,10 @@ const MyPage = () => {
       } else {
         console.error("User ID not available");
       }
-      // setIsLoading(false);
+      setIsLoading(false);
     } catch (error) {
       console.error("Error fetching user profile:", error);
+      setIsLoading(false);
     }
   };
 
@@ -100,90 +108,29 @@ const MyPage = () => {
       setAvatarUrl(url); // 미리보기 업데이트
     }
   };
-
+  const handleEdit = () => {
+    setIsEdited(!isEdited);
+  };
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[1200px] flex justify-center items-center h-full">
-        <div className="w-1/3 h-5/6 bg-black bg-opacity-50 shadow-xl p-3 m-4 rounded-lg">
-          <div className="px-10 pt-10">
-            <label
-              htmlFor="fileInput"
-              className="flex justify-center items-center"
-            >
-              <img
-                src={avatarUrl}
-                alt="Profile"
-                className="rounded-full w-36 h-36"
-              />
-            </label>
-            {isEdited && (
-              <input
-                type="file"
-                id="fileInput"
-                accept="image/*"
-                onChange={handleAvatarChange}
-                className="hidden cursor-pointer border-b-2 border-info"
-              />
-            )}
-          </div>
-
-          <div className="mt-6 items-center text-center p-3">
-            <h2 className="font-bold text-lg">{email}</h2>
-            {isEdited ? (
-              <input
-                type="text"
-                value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
-                className="mt-4 border-b-2 border-info bg-black bg-opacity-50 font-medium text-lg"
-              />
-            ) : (
-              <h2 className="mt-4 font-medium text-lg">{nickname}</h2>
-            )}
-            <h2 className="mt-8 font-bold text-lg">{birth}</h2>
-            <div className="mt-48 flex justify-center gap-4">
-              {isEdited ? (
-                <>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => updateProfile()}
-                  >
-                    저장
-                  </button>
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => setIsEdited(false)}
-                  >
-                    취소
-                  </button>
-                </>
-              ) : (
-                <button
-                  className="btn btn-primary"
-                  onClick={() => setIsEdited(true)}
-                >
-                  수정
-                </button>
-              )}
-            </div>
-          </div>
+      {isLoading ? (
+        <span className="loading loading-dots loading-lg"></span>
+      ) : (
+        <div className="w-[1200px] flex justify-center items-center h-full">
+          <Profile
+            avatarUrl={avatarUrl}
+            handleAvatarChange={handleAvatarChange}
+            isEdited={isEdited}
+            handleEdit={handleEdit}
+            nickname={nickname}
+            setNickname={setNickname}
+            birth={birth}
+            email={email}
+            updateProfile={updateProfile}
+          />
+          <PostSection userPosts={userPosts} />
         </div>
-        <div className="w-10/12 h-5/6 bg-black bg-opacity-50 shadow-xl p-6 m-4 rounded-lg">
-          <div className="p-3">
-            <h2 className="font-bold text-lg">내 게시글 보기</h2>
-            <HoverEffect
-              items={userPosts.map((post) => ({
-                title: post.title,
-                description: post.content, // 간단한 내용으로 카드에 표시
-                link: `/board/${post.id}`, // 게시물 상세 페이지 링크
-              }))}
-            />
-          </div>
-          <div className="p-3">
-            <h2 className="font-bold text-lg">❤️ 좋아요 목록 보기</h2>
-            <p>hi</p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
