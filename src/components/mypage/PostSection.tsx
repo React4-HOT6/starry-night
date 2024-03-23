9ab91f91f8a64de7119eda263a923ff43bc1a435
yeriniii <yerini000303@naver.com>
2024-03-23 20:46:37 +0900

    Feat:mypage의 내 게시글 보기부분 사진 넣기

--- src/components/mypage/PostSection.tsx
+++ src/components/mypage/PostSection.tsx
@@ -3,22 +3,21 @@ import { HoverEffect } from "./CardHoverUI";
 import { Tables } from "@/types/database.types";
 type Board = Tables<"board">;
 const PostSection = ({ userPosts }: { userPosts: Board[] }) => {
+  const hoverItems = userPosts.map((post) => {
+    const firstImage = post.images?.[0]; // 첫 번째 이미지
+    return {
+      images: firstImage,
+      title: post.title,
+      description: post.content, // 간단한 내용으로 카드에 표시
+      link: `/board/detail/${post.id}`, // 게시물 상세 페이지 링크
+    };
+  });
   return (
     <>
-      <div className="p-3">
+      <div className="p-3 ">
         <h2 className="font-bold text-lg">내 게시글 보기</h2>
-        <HoverEffect
-          items={userPosts.map((post) => ({
-            title: post.title,
-            description: post.content, // 간단한 내용으로 카드에 표시
-            link: `/board/${post.id}`, // 게시물 상세 페이지 링크
-          }))}
-        />
+        <HoverEffect items={hoverItems} />
       </div>
-      {/* <div className="p-3">
-        <h2 className="font-bold text-lg">❤️ 좋아요 목록 보기</h2>
-        <p>hi</p>
-      </div> */}
     </>
   );
 };
