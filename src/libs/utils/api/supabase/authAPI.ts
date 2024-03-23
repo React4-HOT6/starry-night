d57d07d23a92a1bc5cfcc9331060fb56740cb0c7
baram55 <asd7973@gmail.com>
2024-03-24 04:52:43 +0900

    Feat : authAPI 유저 정보 불러오기

--- src/libs/utils/api/supabase/authAPI.ts
+++ src/libs/utils/api/supabase/authAPI.ts
@@ -12,3 +12,16 @@ export const getUserId = async () => {
   console.log(user!.id); //NOTE - 테스트 코드
   return { status: "success", result: user!.id } as const;
 };
+
+export const getUserInfo = async () => {
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error) {
+    console.log(error); //NOTE - 테스트 코드
+    return { status: "fail", result: error } as const;
+  }
+  console.log(user!.id); //NOTE - 테스트 코드
+  return { status: "success", result: user } as const;
+};
